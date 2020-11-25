6d9d01b228f483dedb51984678152c61ecd6214b
Sobol2002 <ilja-sobol-sobol.sobol@yandex.ru>
2020-11-25 08:22:24 +0300

    0

--- src/test/java/com/mycompany/kurs/BathroomTest.java
+++ src/test/java/com/mycompany/kurs/BathroomTest.java
@@ -1,7 +1,6 @@
 package com.mycompany.kurs;
 
 import org.junit.Assert;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,30 +21,15 @@ public class BathroomTest {
     }
     @org.junit.Test
     public void deleteTube() throws Exception {
-        int z=1;
         Bathroom bathroom=new Bathroom();
         bathroom.addTube("f",1,1,PLUMBING);
         bathroom.deleteTube("f");
-         if (bathroom.tubeisEmpty()){
-             z=0;
-         }
-         Assert.assertEquals(0,z);
+        Assert.assertTrue(bathroom.tubeisEmpty());
     }
-    @org.junit.Test
-    public void addFiting() throws Exception {
-        Bathroom bathroom=new Bathroom();
-        bathroom.addFitting("f",1,1,PLUMBING);
-        Assert.assertNotNull(bathroom);
-    }
-    @org.junit.Test
-    public void deleteFitting() throws Exception {
-        int z=1;
-        Bathroom bathroom=new Bathroom();
-        bathroom.addFitting("f",1,1,PLUMBING);
-        bathroom.deleteTube("f");
-        if (bathroom.tubeisEmpty()){
-            z=0;
-        }
-        Assert.assertEquals(0,z);
-    }
+//    @org.junit.Test
+//    public void addFiting() throws Exception {
+//        Bathroom bathroom=new Bathroom();
+//        bathroom.addFitting("f",1,1,PLUMBING);
+//        Assert.assertNotNull(bathroom);
+//    }
 }
